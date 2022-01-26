c5c48a760509c786df1523631b02ac10601c8d2d
curly08 <mattynoakes08@gmail.com>
2022-01-26 15:15:28 -0500

    Add game module with #playRound, #checkForWin, and #checkForDraw

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-// Game
-
-const game = (() => {
-  // create players
-
-  // alternate turns
-
-  // declare win/draw
-
-  // play again?
-});
-
-// Gameboard
-
 const gameboard = (() => {
   const board = [];
 
@@ -34,16 +20,60 @@ const gameboard = (() => {
   };
 })();
 
-// Player
-
 const player = (name, piece) => {
-  // prompt to place piece
   const playTurn = () => {
-    let square = prompt("Where do you want to place your piece?");
-    if (gameboard.addPiece(piece, square) === 'failure') {
+    const square = prompt(`${name}, where do you want to place your piece?`);
+    if (parseInt(square) >= 1 && parseInt(square) <= 9 && gameboard.board[(parseInt(square) - 1)] === undefined) {
+      gameboard.addPiece(piece, (parseInt(square) - 1));
+    } else {
       playTurn();
     };
   };
 
   return { name, piece, playTurn };
 };
+
+const game = (() => {
+  const checkForDraw = (board) => {
+    if (board.length === 9 && !board.includes('undefined') && checkForWin(board) === false) {
+      gameStatus = 'draw';
+      alert(`It's a draw!`);
+    };  
+  };
+
+  const checkForWin = (board) => {
+    const winningScenarios = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]]
+    ];
+
+    winningScenarios.forEach(scenario => {
+      if (scenario.every(piece => piece === currentPlayer.piece)) {
+        gameStatus = 'win';
+        alert(`Congrats ${currentPlayer.name}! You won!`);
+      };
+    });
+  };
+
+  const playerOne = player(prompt("Player one, what is your name?"), 'X');
+  const playerTwo = player(prompt("Player two, what is your name?"), 'O');
+  let currentPlayer = playerOne;
+  let gameStatus = 'not over';
+
+  while (gameStatus === 'not over') {
+    currentPlayer.playTurn();
+    checkForWin(gameboard.board);
+    checkForDraw(gameboard.board);
+    if (currentPlayer === playerOne) {
+      currentPlayer = playerTwo;
+    } else {
+      currentPlayer = playerOne;
+    };
+  };
+});
